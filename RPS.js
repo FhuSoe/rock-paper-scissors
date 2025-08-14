32050e81fee17bbe6e698965b0fb6d57ae2523da
FhuSoe <rnurmarianto@yahoo.com>
2025-08-14 18:01:35 +0700

    Added a comment to think about the code

--- RPS.js
+++ RPS.js
@@ -1,4 +1,6 @@
-
+//For return value is between 1,2 ,and 3
+//Convert the number to the a string
+//Tried without using an Array
 
 function getComputerChoice() {
   return Math.floor(Math.random() * 3) + 1;
