e92cae15b4cc9aa48afc0994e0680ca1d76a4e7f
FhuSoe <rnurmarianto@yahoo.com>
2025-08-15 14:10:47 +0700

    Defined the parameters for the function Syntax

--- RPS.js
+++ RPS.js
@@ -36,3 +36,8 @@ let computerScore = 0;
 function playRound(humanChoice, computerChoice) {
   
 }
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
